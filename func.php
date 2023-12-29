6529c2bf2e4750369e7f48c979d91362ed632c30
Null <kelanidarasimi9@gmail.com>
2023-12-29 18:10:07 +0100

    redid

--- func.php
+++ func.php
@@ -9,14 +9,22 @@ function longdates($timestamp)
 }
 
 
-// global variable scope
+// variable scope
 // variables in php by default are local scope which means that
 // non functional variable cannot be accessed inside of a function
 // except specified
 
 $temp = "The date is ";
-echo $temp . longdate(time());
+#echo $temp . longdate(time());
 
 function longdate($timestamp) {
     return (string) date("l F js Y", $timestamp);
+}
+
+// annother way is to pass the variable as a second argument
+
+echo newdate($temp, time());
+
+function newdate($temp, $timestamp) {
+    return $temp . date("l F js Y", $timestamp);
 }
