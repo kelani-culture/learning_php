ab4f6af497c097c2f50caea69a27dc01174308f3
Null <kelanidarasimi9@gmail.com>
2024-01-02 17:37:38 +0100

    learn about the unpack operator

---
+++ func2.php
@@ -0,0 +1,20 @@
+<?php
+// understanding parameters
+declare(strict_types= 1);
+
+// the union allows you to pipe multiple expected datatypes together
+ function foo(int|float $x, int|float $y = 0) {
+    return $x + $y;
+ }
+
+#echo foo(10, 5.0);
+
+// variadic function accepts variadic number of arguments
+// the splat ...operators allows us to take in multiple argument
+/// the splat operator also aloows you to unpack an array
+function sum(int|float...$numbers): int|float {
+    return array_sum($numbers);
+}
+
+$arr = [1, 2, 3, 4, 5, 6];
+echo sum(...$arr);
