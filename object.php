85c9c180a048731fde209ed7225f5c2416250516
Null <kelanidarasimi9@gmail.com>
2024-01-04 16:42:35 +0100

    understand static variable and constant

--- object.php
+++ object.php
@@ -1,13 +1,6 @@
 <?php
 // understanding oop in php
 
-$object1 = new User();
-$object1->name = "Alice";
-$object2 = clone $object1;
-$object2->name = "Any";
-
-echo "object1 name = " . $object1->name ."\n";
-echo "object2 name = " . $object2->name . "\n";
 class User {
     public $name;
 
@@ -15,3 +8,11 @@ class User {
         echo "Save User code goes here";
     }
 }
+
+$object1 = new User();
+$object1->name = "Alice";
+$object2 = clone $object1;
+$object2->name = "Any";
+
+echo "object1 name = " . $object1->name ."\n";
+echo "object2 name = " . $object2->name . "\n";
