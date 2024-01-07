d9c28e6553c7678da3b9d9dabd4fbf98a96dd923
Null <kelanidarasimi9@gmail.com>
2024-01-07 17:46:48 +0100

    understanding the printf function in php

--- practical.php
+++ practical.php
@@ -1,2 +1,15 @@
 <?php
-printf("My name is %s, i'm %d years old, which is %X in hexadecimal", 'simon', 32, 33);
+printf("My name is %s, i'm %d years old, which is %X in hexadecimal\n", 'simon', 32, 33);
+
+// pad to 15 spaces
+printf("The result is $%15f\n", 123.42 / 12);
+
+// pad to 15 spaces fill with zeros
+printf("The result is $%015f\n", 123.42 / 12);
+
+// pad to 15 spaces 2 decimal places precision
+printf("The result is $%15.2ff\n", 123.42 / 12);
+ 
+printf("The result is $%015.2f\n", 123.42 / 12);
+
+printf("The result is $%'#15.2f\n", 123.42 / 12);
