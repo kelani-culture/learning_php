4ac0e8cac7a90053302474f33c6403754c6eb627
Null <kelanidarasimi9@gmail.com>
2024-01-07 20:55:59 +0100

    basic operation on files in php

--- file_handling.php
+++ file_handling.php
@@ -2,23 +2,38 @@
 // understanding file in php
 
 // checking whether a file exists
-if (file_exists("testfile.txt")) {
-    $fh = fopen("testfile.txt", 'r') or die('File does not exist or you lack permission to read');
+function files() {
+    if (file_exists("testfile.txt")) {
+        $fh = fopen("testfile.txt", 'r') or die('File does not exist or you lack permission to read');
 
-    $line = fread($fh, 10);
-    fclose($fh);
-    echo $line;
+        $filename = 'testfile.txt';
+        $line = fread($fh, filesize($filename));
+
+        fclose($fh);
+        echo $line;
+    }
+    else {
+        $fh = fopen("testfile.txt", "w") or die("Failed to create a file");
+
+        $text = <<<_END
+        This is a file
+        that I just created for testing
+        only please do not open
+        _END;
+        fwrite($fh, $text);
+        fclose($fh);
+        echo "File 'testfile.txt' written successfully";
+    }
+}
+#files();
+
+// copying file in php
+if (file_exists('testfile.txt')) {
+    copy('testfile.txt', 'testfile2.txt');
+    echo "copy file successful\n";
 }
 
-else {
-    $fh = fopen("testfile.txt", "w") or die("Failed to create a file");
-
-    $text = <<<_END
-    This is a file
-    that I just created for testing
-    only please do not open
-    _END;
-    fwrite($fh, $text);
-    fclose($fh);
-    echo "File 'testfile.txt' written successfully";
-};
+// rename function
+if (!rename('testfile2.txt', 'testfile2.new')) {
+    echo "could not rename file";
+} else echo "File successfully reneamed to 'testfile2.new";
