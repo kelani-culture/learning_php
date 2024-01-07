0851866c8917f616da7fdb468edcd0b95b08cf3b
Null <kelanidarasimi9@gmail.com>
2024-01-07 19:57:01 +0100

    string padding with printf in php

--- practical.php
+++ practical.php
@@ -13,3 +13,17 @@ printf("The result is $%15.2ff\n", 123.42 / 12);
 printf("The result is $%015.2f\n", 123.42 / 12);
 
 printf("The result is $%'#15.2f\n", 123.42 / 12);
+
+
+// string padding
+$h='Rasmus';
+printf("[%s]\n",                   $h);
+printf("[%12s]\n",                   $h);
+printf("[%-12s]\n",                   $h);
+printf("[%012s]\n",                   $h);
+printf("[%'#012s]\n",                   $h);
+
+$d = 'Rsamus Lerdorf';
+printf("[%12.8s]\n",          $d);
+printf("[%-12.12s]\n",          $d);
+printf("[%-'@12.8s]\n",          $d);
