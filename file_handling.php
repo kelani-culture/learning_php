a45b335a3b891b774d7cef6d9b99b0f5e18898d0
Null <kelanidarasimi9@gmail.com>
2024-01-08 15:03:47 +0100

    learn about flock

--- file_handling.php
+++ file_handling.php
@@ -33,7 +33,36 @@ if (file_exists('testfile.txt')) {
     echo "copy file successful\n";
 }
 
-// rename function
-if (!rename('testfile2.txt', 'testfile2.new')) {
-    echo "could not rename file";
-} else echo "File successfully reneamed to 'testfile2.new";
+function  file_operation(){
+    if (!rename('testfile2.txt', 'testfile2.new')) {
+        echo "could not rename file\n";
+    } else echo "File successfully reneamed to 'testfile2.new\n";
+
+    // unlink function
+    if (!unlink('testfile2.new')) echo "could not delete file";
+    else echo "File testfile2.new successfully deleted";
+
+    // update files
+    $fh = fopen("testfile.txt", 'r+') or die("Failed to open file");
+
+    $text = <<<_END
+    what a new line to write at the end
+    so sorry told you not to read it full of shit
+    _END;
+
+    fseek($fh, 0, SEEK_END);
+    fwrite($fh, "$text") or die("could not write to file");
+    fclose($fh);
+    echo "File 'testfile.txt' successfully updated";
+
+}
+
+$fh = fopen("testfile.txt", 'r+') or die("Failed  to open file");
+$text = fgets($fh);
+
+if (flock($fh, LOCK_EX)) {
+    fseek($fh, "$text") or die("Could not write to file");
+    flock($fh, LOCK_UN);
+}
+fclose($fh);
+echo "File 'testgile.txt' successfully updated";
